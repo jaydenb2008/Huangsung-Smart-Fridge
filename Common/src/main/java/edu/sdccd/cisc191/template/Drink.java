ef41b7df1a1623bdd386b0165fbf9d49dc9e4a45
jaydenb2008 <jjeromebui2008@gmail.com>
2025-03-11 14:22:34 -0700

    add method to validate date input is formatted in MM-dd-yyyy + passes unit tests

--- Common/src/main/java/edu/sdccd/cisc191/template/Drink.java
+++ Common/src/main/java/edu/sdccd/cisc191/template/Drink.java
@@ -3,8 +3,8 @@ package edu.sdccd.cisc191.template;
 public class Drink extends FoodItem {
     private boolean isOpened;
 
-    public Drink(String name, String foodType, double quantityLeft, boolean isOpened) {
-        super(name, foodType, quantityLeft);
+    public Drink(String name, String foodType, double quantityLeft, String expirationDate, boolean isOpened) {
+        super(name, foodType, quantityLeft, expirationDate);
         this.isOpened = isOpened;
     }
 
