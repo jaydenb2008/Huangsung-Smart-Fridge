45ffbe1ea4989e26af93ad1625a02d8b09df4f90
jaydenb2008 <jjeromebui2008@gmail.com>
2025-03-10 21:47:23 -0700

    create storage + class outline

---
+++ Common/src/main/java/edu/sdccd/cisc191/template/Storage.java
@@ -0,0 +1,11 @@
+package edu.sdccd.cisc191.template;
+
+import java.util.ArrayList;
+
+public class Storage {
+    private ArrayList<String> storage = new ArrayList<String>();
+
+    //TODO create and format expiration date + old expiration date methods
+    //TODO add everything to the arrayList
+    //TODO convert arrayList to a 2D String array
+}
