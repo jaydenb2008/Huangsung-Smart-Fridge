a117dfc91589cbf4c20d3919a010d5d136e2ab87
jaydenb2008 <jjeromebui2008@gmail.com>
2025-03-11 14:22:34 -0700

    add method to validate date input is formatted in MM-dd-yyyy + passes unit tests

--- Common/src/main/java/edu/sdccd/cisc191/template/FoodItem.java
+++ Common/src/main/java/edu/sdccd/cisc191/template/FoodItem.java
@@ -1,15 +1,22 @@
 package edu.sdccd.cisc191.template;
 
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class FoodItem {
     private String name;
     private String foodType;
     private double quantityLeft;
+    private String expirationDate;
 
 
-    public FoodItem(String name, String foodType, double quantityLeft) {
+    public FoodItem(String name, String foodType, double quantityLeft, String expirationDate) {
         this.name = name;
         this.foodType = foodType;
         this.quantityLeft = quantityLeft;
+        this.expirationDate = expirationDate;
     }
 
     public String getName() {
@@ -34,5 +41,27 @@ public class FoodItem {
 
     public void setQuantityLeft(double quantityLeft) {
         this.quantityLeft = quantityLeft;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public boolean isDateValid(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+
+        try {
+            LocalDate expiration = LocalDate.parse(date, formatter);
+            System.out.println("This is a valid date");
+            return true;
+        } catch (DateTimeParseException e) {
+            System.out.println("Invalid date!");
+        }
+
+        return false;
     }
 }
--- Common/src/test/java/edu/sdccd/cisc191/template/HuangsungTest.java
+++ Common/src/test/java/edu/sdccd/cisc191/template/HuangsungTest.java
@@ -7,21 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class HuangsungTest {
     @Test
     void instantiateFoodItem() {
-        FoodItem eggs = new FoodItem("Eggs", "Poultry", 12);
+        FoodItem eggs = new FoodItem("Eggs", "Poultry", 12, "03-11-2025");
 
         assertFalse(eggs.getName().isEmpty());
         assertFalse(eggs.getFoodType().isEmpty());
         assertTrue(eggs.getQuantityLeft() > 0);
+
+        assertTrue(eggs.isDateValid(eggs.getExpirationDate()));
     }
 
     @Test
     void instantiateDrink() {
-        Drink milk = new Drink("Milk", "Dairy", 0.5, true);
+        Drink milk = new Drink("Milk", "Dairy", 0.5, "2025-03-11", true);
 
         assertInstanceOf(FoodItem.class, milk);
 
         assertFalse(milk.getName().isEmpty());
         assertFalse(milk.getFoodType().isEmpty());
+
+        assertFalse(milk.isDateValid(milk.getExpirationDate()));
 
         if (!milk.isOpened()) {
             assertEquals(1, milk.getQuantityLeft());
