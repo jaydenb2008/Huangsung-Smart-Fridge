f1b1970d32c49b36ac99262c7aea6d46c4d190af
jaydenb2008 <jjeromebui2008@gmail.com>
2025-03-16 15:38:58 -0700

    create storage with add/remove operations; unit test passes

--- Common/src/main/java/edu/sdccd/cisc191/template/Storage.java
+++ Common/src/main/java/edu/sdccd/cisc191/template/Storage.java
@@ -1,11 +1,76 @@
 package edu.sdccd.cisc191.template;
 
-import java.util.ArrayList;
 
 public class Storage {
-    private ArrayList<String> storage = new ArrayList<String>();
+    private FoodItem[][] fridge;
+    private int fridgeSize;
+    private int itemCount;
 
-    //TODO create and format expiration date + old expiration date methods
-    //TODO add everything to the arrayList
-    //TODO convert arrayList to a 2D String array
+    public Storage(int fridgeSize) {
+        this.fridge = new FoodItem[fridgeSize][1];
+        this.fridgeSize = fridgeSize;
+        this.itemCount = 0;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public int getFridgeSize() {
+        return fridgeSize;
+    }
+
+    public FoodItem getFoodItem(int index) {
+        if (index >= 0 && index < itemCount) {
+            return fridge[index][0];
+        }
+        return null;
+    }
+
+    public void addFood(FoodItem food) {
+        if (itemCount == fridgeSize) {
+            resizeArray();
+        }
+
+        fridge[itemCount][0] = food;
+        itemCount++;
+    }
+
+    public void removeFood(String name) {
+        boolean found = false;
+        for (int i = 0; i < itemCount; i++) {
+            //if index is not empty and name matches, remove item
+            if (fridge[i][0] != null && fridge[i][0].getName().equals(name)) {
+                found = true;
+                fridge[i][0] = null;
+            }
+
+            //if the item was not the last item in the array, shift all proceeding items to the left to fill in gaps
+            if (found && i < itemCount - 1) {
+                fridge[i][0] = fridge[i+1][0];
+            }
+
+            //ensure that there is one less item in the fridge before updating the counter
+            if (found) {
+                fridge[itemCount - 1][0] = null;
+                itemCount--;
+                System.out.println(name + " removed.");
+            } else {
+                System.out.println("Item not found");
+            }
+        }
+    }
+
+
+    public void resizeArray() {
+        int newSize = fridgeSize + 5;
+        FoodItem[][] newFridge = new FoodItem[newSize][1];
+
+        for (int i = 0; i < fridgeSize; i++) {
+            newFridge[i][0] = fridge[i][0];
+        }
+
+        this.fridge = newFridge;
+        this.fridgeSize = newSize;
+    }
 }
--- Common/src/test/java/edu/sdccd/cisc191/template/HuangsungTest.java
+++ Common/src/test/java/edu/sdccd/cisc191/template/HuangsungTest.java
@@ -19,12 +19,44 @@ class HuangsungTest {
 
         assertFalse(milk.isExpired(milk.getExpirationDate()));
 
+        milk.setExpirationDate(Drink.convertToDate("02-12-2025"));
+        assertTrue(milk.isExpired(milk.getExpirationDate()));
+
         if (!milk.isOpened()) {
             assertEquals(1, milk.getQuantityLeft());
         } else {
             assertFalse(milk.getQuantityLeft() > 1);
             assertFalse(milk.getQuantityLeft() < 0);
         }
+    }
 
+    @Test
+    void testStorage() {
+        Storage testFridge = new Storage(3);
+
+        FoodItem tomatoes = new FoodItem("Tomatoes", "Vegetable", 3.0f, FoodItem.convertToDate("03-23-2025"));
+        FoodItem eggs = new FoodItem("Eggs", "Poultry", 12.0f, FoodItem.convertToDate("03-16-2025"));
+        Drink milk = new Drink("Milk", "Dairy", 0.5f, Drink.convertToDate("04-16-2025"), true);
+        Drink lemonade = new Drink("Lemonade", "Juice", 1.0f, Drink.convertToDate("05-02-2025"), false);
+
+        testFridge.addFood(tomatoes);
+        testFridge.addFood(eggs);
+        testFridge.addFood(milk);
+
+        assertEquals(3, testFridge.getItemCount());
+        assertEquals("Eggs", testFridge.getFoodItem(1).getName());
+
+        testFridge.removeFood("Eggs");
+        assertEquals(2, testFridge.getItemCount());
+        assertEquals("Milk", testFridge.getFoodItem(1).getName());
+        assertNull(testFridge.getFoodItem(2));
+
+        testFridge.addFood(eggs);
+        testFridge.addFood(lemonade);
+        assertEquals(4, testFridge.getItemCount());
+        assertEquals(8, testFridge.getFridgeSize());
+
+        testFridge.removeFood("Bacon");
+        assertEquals(4, testFridge.getItemCount());
     }
 }
