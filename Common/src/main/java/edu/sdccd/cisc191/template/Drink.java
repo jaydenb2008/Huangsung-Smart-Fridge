a542e74025c86326a16a5755b93de5893257b9a4
jaydenb2008 <jjeromebui2008@gmail.com>
2025-03-16 13:17:40 -0700

    change data types of expirationDate, quantityLeft; all tests pass

--- Common/src/main/java/edu/sdccd/cisc191/template/Drink.java
+++ Common/src/main/java/edu/sdccd/cisc191/template/Drink.java
@@ -1,9 +1,11 @@
 package edu.sdccd.cisc191.template;
 
+import java.util.Date;
+
 public class Drink extends FoodItem {
     private boolean isOpened;
 
-    public Drink(String name, String foodType, double quantityLeft, String expirationDate, boolean isOpened) {
+    public Drink(String name, String foodType, float quantityLeft, Date expirationDate, boolean isOpened) {
         super(name, foodType, quantityLeft, expirationDate);
         this.isOpened = isOpened;
     }
--- Common/src/test/java/edu/sdccd/cisc191/template/HuangsungTest.java
+++ Common/src/test/java/edu/sdccd/cisc191/template/HuangsungTest.java
@@ -2,30 +2,22 @@ package edu.sdccd.cisc191.template;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HuangsungTest {
-    @Test
-    void instantiateFoodItem() {
-        FoodItem eggs = new FoodItem("Eggs", "Poultry", 12, "03-11-2025");
-
-        assertFalse(eggs.getName().isEmpty());
-        assertFalse(eggs.getFoodType().isEmpty());
-        assertTrue(eggs.getQuantityLeft() > 0);
-
-        assertTrue(eggs.isDateValid(eggs.getExpirationDate()));
-    }
 
     @Test
     void instantiateDrink() {
-        Drink milk = new Drink("Milk", "Dairy", 0.5, "2025-03-11", true);
+        Drink milk = new Drink("Milk", "Dairy", 0.5f, Drink.convertToDate("04-16-2025"), true);
 
         assertInstanceOf(FoodItem.class, milk);
 
         assertFalse(milk.getName().isEmpty());
         assertFalse(milk.getFoodType().isEmpty());
 
-        assertFalse(milk.isDateValid(milk.getExpirationDate()));
+        assertFalse(milk.isExpired(milk.getExpirationDate()));
 
         if (!milk.isOpened()) {
             assertEquals(1, milk.getQuantityLeft());
